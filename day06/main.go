7bcccc2caab17eed471d2020754815fd6b45aa7d
Matt Bargenquast <matt.bargenquast@gmail.com>
2024-12-06 21:08:10 +1000

    Day 6 part 2 added

--- day06/main.go
+++ day06/main.go
@@ -10,13 +10,22 @@ import (
 type Player struct {
 	direction helper.Direction
 	position  helper.Coord
+	visited   map[string]bool
 }
 
-func (p *Player) move(grid [][]bool) bool {
+func (p *Player) move(grid [][]bool) (validMove bool, inLoop bool) {
+	visitKey := fmt.Sprintf("%v,%v,%v", p.position.X, p.position.Y, p.direction)
+	if _, ok := p.visited[visitKey]; !ok {
+		p.visited[visitKey] = true
+	} else {
+		// we're at the same position we've been, moving in the same direction
+		// so this has to be a loop...... right...?
+		return true, true
+	}
 
 	nextPos := p.position.Move(p.direction)
 	if !nextPos.IsValid(len(grid[0]), len(grid)) {
-		return false
+		return false, false
 	}
 	if grid[nextPos.Y][nextPos.X] {
 		// hit an object, turn
@@ -24,7 +33,7 @@ func (p *Player) move(grid [][]bool) bool {
 	} else {
 		p.position = nextPos
 	}
-	return true
+	return true, false
 }
 
 func readData(lines []string) (r [][]bool, start helper.Coord) {
@@ -46,22 +55,57 @@ func readData(lines []string) (r [][]bool, start helper.Coord) {
 
 func partone(lines []string) (r int, err error) {
 	grid, startcoord := readData(lines)
+	visited, _ := simulate(grid, startcoord)
+	return visited, nil
+}
+
+func simulate(grid [][]bool, startcoord helper.Coord) (visited int, isloop bool) {
 	player := Player{
 		direction: helper.NORTH,
 		position:  startcoord,
+		visited:   make(map[string]bool),
 	}
 	seen := make(map[helper.Coord]bool)
 	for {
 		seen[player.position] = true
-		if !player.move(grid) {
+		if valid, looped := player.move(grid); !valid || looped {
+			if looped {
+				isloop = true
+			}
 			break
 		}
 	}
-	return len(seen), nil
+	return len(seen), isloop
 }
 
 func parttwo(lines []string) (r int, err error) {
-	return 0, nil
+	grid, startcoord := readData(lines)
+
+	for y, row := range grid {
+		for x, v := range row {
+			if v || (y == startcoord.Y && x == startcoord.X) {
+				// can't mess with this grid sapce
+				continue
+			}
+			newgrid := copyGrid(grid)
+			newgrid[y][x] = true
+			_, loop := simulate(newgrid, startcoord)
+			if loop {
+				r += 1
+			}
+
+		}
+	}
+	return r, nil
+}
+
+func copyGrid(grid [][]bool) [][]bool {
+	retGrid := make([][]bool, len(grid))
+	for i := range grid {
+		retGrid[i] = make([]bool, len(grid[i]))
+		copy(retGrid[i], grid[i])
+	}
+	return retGrid
 }
 
 func main() {
