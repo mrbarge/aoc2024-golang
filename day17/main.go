2123f3354589d4aca541826a0c23322452527e2c
Matt Bargenquast <matt.bargenquast@gmail.com>
2025-01-06 22:08:06 +1000

    Day 17 p2

--- day17/main.go
+++ day17/main.go
@@ -24,12 +24,12 @@ const (
 )
 
 type CPU struct {
-	A       int
-	B       int
-	C       int
-	I       int
-	Program []int
-	Output  []int
+	A       uint64
+	B       uint64
+	C       uint64
+	I       uint64
+	Program []uint64
+	Output  []uint64
 }
 
 func readData(lines []string) CPU {
@@ -38,29 +38,27 @@ func readData(lines []string) CPU {
 		B:       0,
 		C:       0,
 		I:       0,
-		Program: make([]int, 0),
-		Output:  make([]int, 0),
+		Program: make([]uint64, 0),
+		Output:  make([]uint64, 0),
 	}
-	cpu.A, _ = strconv.Atoi(strings.Split(lines[0], " ")[2])
-	cpu.B, _ = strconv.Atoi(strings.Split(lines[1], " ")[2])
-	cpu.C, _ = strconv.Atoi(strings.Split(lines[2], " ")[2])
+	a, _ := strconv.Atoi(strings.Split(lines[0], " ")[2])
+	b, _ := strconv.Atoi(strings.Split(lines[1], " ")[2])
+	c, _ := strconv.Atoi(strings.Split(lines[2], " ")[2])
+	cpu.A = uint64(a)
+	cpu.B = uint64(b)
+	cpu.C = uint64(c)
 	regstr := strings.Split(strings.Split(lines[3], " ")[1], ",")
 	for _, r := range regstr {
 		ri, _ := strconv.Atoi(r)
-		cpu.Program = append(cpu.Program, ri)
+		cpu.Program = append(cpu.Program, uint64(ri))
 	}
 	return cpu
 }
 
-func (c *CPU) Instruction(seen map[string]struct{}) bool {
-	if c.I >= len(c.Program)-1 {
+func (c *CPU) Instruction() bool {
+	if c.I >= uint64(len(c.Program)-1) {
 		return false
 	}
-	if _, ok := seen[c.seen()]; ok {
-		// been in this state before and we know it's not good
-		return false
-	}
-	seen[c.seen()] = struct{}{}
 
 	operand := c.Program[c.I+1]
 
@@ -92,21 +90,21 @@ func (c *CPU) Instruction(seen map[string]struct{}) bool {
 	return true
 }
 
-func (c *CPU) adv(o int) {
+func (c *CPU) adv(o uint64) {
 	n := c.A
-	d := int(math.Pow(float64(2), float64(c.combo(o))))
+	d := uint64(math.Pow(float64(2), float64(c.combo(o))))
 	c.A = n / d
 }
 
-func (c *CPU) bxl(o int) {
+func (c *CPU) bxl(o uint64) {
 	c.B = c.B ^ o
 }
 
-func (c *CPU) bst(o int) {
+func (c *CPU) bst(o uint64) {
 	c.B = c.combo(o) % 8
 }
 
-func (c *CPU) jnz(o int) bool {
+func (c *CPU) jnz(o uint64) bool {
 	if c.A == 0 {
 		return false
 	}
@@ -114,31 +112,27 @@ func (c *CPU) jnz(o int) bool {
 	return true
 }
 
-func (c *CPU) bxc(o int) {
+func (c *CPU) bxc(o uint64) {
 	c.B = c.B ^ c.C
 }
 
-func (c *CPU) out(o int) {
+func (c *CPU) out(o uint64) {
 	c.Output = append(c.Output, c.combo(o)%8)
 }
 
-func (c *CPU) bdv(o int) {
+func (c *CPU) bdv(o uint64) {
 	n := c.A
-	d := int(math.Pow(float64(2), float64(c.combo(o))))
+	d := uint64(math.Pow(float64(2), float64(c.combo(o))))
 	c.B = n / d
 }
 
-func (c *CPU) cdv(o int) {
+func (c *CPU) cdv(o uint64) {
 	n := c.A
-	d := int(math.Pow(float64(2), float64(c.combo(o))))
+	d := uint64(math.Pow(float64(2), float64(c.combo(o))))
 	c.C = n / d
 }
 
-func (c *CPU) seen() string {
-	return fmt.Sprintf("%v%v%v%v", c.A, c.B, c.C, c.I)
-}
-
-func (c *CPU) combo(o int) int {
+func (c *CPU) combo(o uint64) uint64 {
 	if o >= 0 && o <= 3 {
 		return o
 	} else if o == 4 {
@@ -148,14 +142,14 @@ func (c *CPU) combo(o int) int {
 	} else if o == 6 {
 		return c.C
 	} else {
-		return -1
+		return 0
 	}
 }
 
 func (c *CPU) PrintOutput() string {
 	soutput := make([]string, len(c.Output))
 	for i, num := range c.Output {
-		soutput[i] = strconv.Itoa(num)
+		soutput[i] = strconv.FormatUint(num, 10)
 	}
 	return strings.Join(soutput, ",")
 }
@@ -163,7 +157,7 @@ func (c *CPU) PrintOutput() string {
 func (c *CPU) PrintProgram() string {
 	soutput := make([]string, len(c.Program))
 	for i, num := range c.Program {
-		soutput[i] = strconv.Itoa(num)
+		soutput[i] = strconv.FormatUint(num, 10)
 	}
 	return strings.Join(soutput, ",")
 }
@@ -178,8 +172,8 @@ func (c *CPU) Clone() CPU {
 		B:       c.B,
 		C:       c.C,
 		I:       c.I,
-		Program: make([]int, 0),
-		Output:  make([]int, 0),
+		Program: make([]uint64, 0),
+		Output:  make([]uint64, 0),
 	}
 	for _, v := range c.Program {
 		r.Program = append(r.Program, v)
@@ -187,36 +181,52 @@ func (c *CPU) Clone() CPU {
 	return r
 }
 
-func partone(lines []string) (r int, err error) {
+func logN(a uint64, n uint64) uint64 {
+	result := math.Log(float64(a)) / math.Log(float64(n))
+	return uint64(result)
+}
+
+func partone(lines []string) (r uint64, err error) {
 	cpu := readData(lines)
-	seen := make(map[string]struct{})
-	for cpu.Instruction(seen) {
+	for cpu.Instruction() {
 		//cpu.PrintState()
 	}
 	fmt.Printf("%v\n", cpu.PrintOutput())
 	return 0, nil
 }
 
-func parttwo(lines []string) (r int, err error) {
+func parttwo(lines []string) (r uint64, err error) {
 	cpu := readData(lines)
 
-	testOutput := cpu.PrintProgram()
-	acounter := 53437163
-	seen := make(map[string]struct{})
+	pow := uint64(0)
+	aVal := uint64(math.Pow(8, float64(len(cpu.Program)-1)))
+
 	for {
-
-		trial := cpu.Clone()
-		trial.A = acounter
-
-		for trial.Instruction(seen) {
+		cpu.A = aVal
+		for cpu.Instruction() {
 		}
-
-		if trial.PrintOutput() == testOutput {
+		if cpu.PrintOutput() == cpu.PrintProgram() {
 			break
 		}
-		acounter++
+
+		pow = logN(aVal, 8)
+		for i := 0; i < len(cpu.Output)-1; i++ {
+			n := cpu.Output[len(cpu.Output)-1-i]
+			programIndex := len(cpu.Program) - 1 - i
+
+			if programIndex < 0 || cpu.Program[programIndex] != n {
+				break
+			}
+			pow--
+		}
+
+		aVal += uint64(math.Pow(8, float64(pow)))
+		cpu.B = 0
+		cpu.C = 0
+		cpu.I = 0
+		cpu.Output = make([]uint64, 0)
 	}
-	return acounter, nil
+	return aVal, nil
 }
 
 func main() {
